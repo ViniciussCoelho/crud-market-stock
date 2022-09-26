0723a13024046b03e325b2f49b2386e0c5fd187d
Vinicius Coelho <vinicius2001@gmail.com>
2022-09-26 20:07:37 -0300

    Arrumando toques finais

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
+
+  validates :name, presence: true, uniqueness: true
+  validates :quantity, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true
+  validates :brand_id, presence: true
+
+
 end
